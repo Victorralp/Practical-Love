f43e8aadf9d3a517d84aa342fef99d9afadb4459
victorralp <victorralph407@gmail.com>
2025-12-22 13:33:08 +0100

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';          // <-- use Vite's defineConfig
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: { '@': '/src' },
